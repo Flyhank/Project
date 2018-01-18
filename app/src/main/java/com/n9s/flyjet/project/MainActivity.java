9079318ec7b442eb3b3cc09751e1d586439026a2
HKW <HKW@yahoo.com>
2018-01-18 16:53:56 +0800

    layout add

--- app/src/main/java/com/n9s/flyjet/project/MainActivity.java
+++ app/src/main/java/com/n9s/flyjet/project/MainActivity.java
@@ -1,13 +1,44 @@
 package com.n9s.flyjet.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.n9s.flyjet.project.data.Phone;
+import com.n9s.flyjet.project.data.PhoneFileDAO;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    public static PhoneFileDAO dao;
+    //以(靜態物件)StudentScoreDAO class宣告變數dao
+    ListView lv;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         dao = new PhoneFileDAO(this);
     }
+
+    public void clicksetting(View v)
+    {
+        Intent it = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(it);
+    }
+
+
+
+
+
 }
+
