359517e0287537c7582cfb54b2c2604f2f0d15d6
Flyha <Flyhank@gmail.com>
2018-02-02 13:54:04 +0800

    20180202_01

--- app/src/main/java/com/n9s/flyjet/project/Main2Activity.java
+++ app/src/main/java/com/n9s/flyjet/project/Main2Activity.java
@@ -1,13 +1,207 @@
 package com.n9s.flyjet.project;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.provider.CalendarContract.CalendarCache.URI;
+import static com.n9s.flyjet.project.MainActivity.dao;
 
 public class Main2Activity extends AppCompatActivity {
+    LocationManager lm;
+    Address addr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
+
+    public void clickPic(View v) {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, 11);
+    }
+
+    public void clickCam(View v) {
+        //Intent i = new Intent(Intent.ACTION_CAMERA_BUTTON, null);     //相機noise出現
+        //this.sendBroadcast(i);
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(it, 123);
+    }
+
+    public void clickRecord(View v) {
+        Intent mi = new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+        startActivity(mi);
+    }
+
+    public void clickFB(View v) {
+        Uri uri = Uri.parse("http://www.facebook.com");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
+
+
+/*    public void clickLine(View v)
+    {
+        private class webViewClient extends WebViewClient
+        {
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                if (url.contains("line.naver.jp"))
+                {
+                    Intent iuri = null;
+                    try
+                    {
+                        iuri = Intent.parseUri(url, 0);
+                    } catch (URISyntaxException e)
+                    {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    startActivity(iuri);
+                }
+                return true;
+            }
+        }
+    }
+    */
+
+    public void clickPhone(View v)
+    {
+        finish();
+    }
+
+    //=====================================================================================================================================
+    public void clickSOS2(View v)
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 321);
+            return;
+        }
+        else
+        {
+            startLoc();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 321)
+        {
+            if (grantResults.length>0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                startLoc();
+            }
+            else
+            {
+            }
+        }
+    }
+
+    public void startLoc()
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new Main2Activity.MyListener());
+    }
+
+    class MyListener implements LocationListener
+    {
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            Geocoder geocoder = new Geocoder(Main2Activity.this);
+            try
+            {
+                List<Address> mylist2 = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                addr = mylist2.get(0);
+                Log.d("LOC", addr.getAddressLine(0));
+
+                Intent smsIntent = new Intent(android.content.Intent.ACTION_VIEW);
+                smsIntent.setType("vnd.android-dir/mms-sms");
+                smsIntent.putExtra("address", dao.getList().get(0).tel.toString()+";"+dao.getList().get(1).tel.toString());
+                smsIntent.putExtra("sms_body", "我需要幫忙!! 位置在: "+addr.getAddressLine(0)+"; (緯度: "+location.getLatitude()+"; "+"經度: "+location.getLongitude()+"). "+", 請快點過來幫我!!!");
+                startActivity(smsIntent);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle)
+        {
+
+        }
+        @Override
+        public void onProviderEnabled(String s)
+        {
+
+        }
+        @Override
+        public void onProviderDisabled(String s)
+        {
+
+        }
+
+
+
+    }
+
+    public void clickNavg(View v)
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 321);
+            return;
+        }
+        else
+        {
+            startLoc();
+        }
+        //Uri uri = Uri.parse("https://www.google.com.tw/maps/dir/220台灣新北市莊敬路8號/220新北市板橋區文化路二段522號");
+        Uri uri = Uri.parse("https://www.google.com.tw/maps/dir/"+addr.getAddressLine(0)+"/220新北市板橋區文化路二段522號");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+
 }
+
+
+
--- app/src/main/java/com/n9s/flyjet/project/MainActivity.java
+++ app/src/main/java/com/n9s/flyjet/project/MainActivity.java
@@ -1,28 +1,40 @@
 package com.n9s.flyjet.project;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.n9s.flyjet.project.data.Phone;
 import com.n9s.flyjet.project.data.PhoneFileDAO;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity
 {
     public static PhoneFileDAO dao;
-    //ListView lv;
-    //DBType dbType;  //enum宣告
     Button btn1, btn2, btn3, btn4, btn5, btn6;
-
     ArrayList<String> phoneNames;   //陣列
     //ArrayAdapter<String> adapter;   //工具
-
-
+    LocationManager lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,12 +42,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = findViewById(R.id.button1);
-        btn2 = findViewById(R.id.button2);
-        btn3 = findViewById(R.id.button3);
-        btn4 = findViewById(R.id.button4);
-        btn5 = findViewById(R.id.button5);
-        btn6 = findViewById(R.id.button6);
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         dao = new PhoneFileDAO(this);
         if(dao.getList().size()==0)
@@ -47,6 +54,19 @@ public class MainActivity extends AppCompatActivity
             dao.add(new Phone(5, "聯絡人名字", "0000000000"));
             dao.add(new Phone(6, "聯絡人名字", "0000000000"));
         }
+    }
+
+    @Override
+    protected void onResume()   //新增資料時,新頁面會蓋住listView頁面進入stop and hale; 新增資料回來後,
+    //listView頁面會resume
+    {
+        super.onResume();
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+        btn4 = findViewById(R.id.button4);
+        btn5 = findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
 
         btn1.setText(dao.getList().get(0).name.toString());
         btn2.setText(dao.getList().get(1).name.toString());
@@ -55,6 +75,43 @@ public class MainActivity extends AppCompatActivity
         btn5.setText(dao.getList().get(4).name.toString());
         btn6.setText(dao.getList().get(5).name.toString());
 
+    }
+
+    public void clickFone1(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(0).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void clickFone2(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(1).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void clickFone3(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(2).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void clickFone4(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(3).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void clickFone5(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(4).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
+    }
+    public void clickFone6(View v)
+    {
+        Uri uri = Uri.parse("tel:"+dao.getList().get(5).tel.toString()); //一定要有"tel:"才行
+        Intent it = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(it);
     }
 
     public void clickContact(View v)
@@ -72,6 +129,88 @@ public class MainActivity extends AppCompatActivity
     public void clickSetting(View v)
     {
         startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+    }
+
+    //=====================================================================================================================================
+    public void clickSOS(View v)
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 321);
+            return;
+        }
+        else
+        {
+            startLoc();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 321)
+        {
+            if (grantResults.length>0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                startLoc();
+            }
+            else
+            {
+            }
+        }
+    }
+
+    private void startLoc()
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyListener());
+    }
+
+    class MyListener implements LocationListener
+    {
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            Geocoder geocoder = new Geocoder(MainActivity.this);
+            try
+            {
+                List<Address> mylist2 = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                Address addr = mylist2.get(0);
+                Log.d("LOC", addr.getAddressLine(0));
+
+                Intent smsIntent = new Intent(android.content.Intent.ACTION_VIEW);
+                smsIntent.setType("vnd.android-dir/mms-sms");
+                smsIntent.putExtra("address", dao.getList().get(0).tel.toString()+";"+dao.getList().get(1).tel.toString());
+                smsIntent.putExtra("sms_body", "我需要幫忙!! 位置在: "+addr.getAddressLine(0)+"; (緯度: "+location.getLatitude()+"; "+"經度: "+location.getLongitude()+"). "+", 請快點過來幫我!!!");
+                startActivity(smsIntent);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle)
+        {
+
+        }
+        @Override
+        public void onProviderEnabled(String s)
+        {
+
+        }
+        @Override
+        public void onProviderDisabled(String s)
+        {
+
+        }
+
     }
 
 
